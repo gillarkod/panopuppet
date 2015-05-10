82fa7adedf8dddf36e6bb73ececbdadababfe5d1
Takeshi Larsson <takeshi.p.larsson@gmail.com>
2015-05-10 20:18:24 +0200

    more test for mk_puppetdb_query

--- tests/test_puppetdb_functions.py
+++ tests/test_puppetdb_functions.py
@@ -5,18 +5,20 @@ from pano.puppetdb.puppetdb import mk_puppetdb_query
 
 
 class CreatePuppetdbQueries(TestCase):
-    def test_single_query(self):
+    def test_single_search_query(self):
         content = {
             'query':
                 {
                     1: '["=","certname","hostname.example.com"]'
                 },
         }
-        expected_results = {'query': '["=","certname","hostname.example.com"]'}
+        expected_results = {
+            'query': '["=","certname","hostname.example.com"]'
+        }
         results = mk_puppetdb_query(content)
         self.assertEqual(expected_results, results)
 
-    def test_double_query_with_operator(self):
+    def test_double_search_query_with_operator(self):
         content = {
             'query':
                 {
@@ -26,11 +28,12 @@ class CreatePuppetdbQueries(TestCase):
                 },
         }
         expected_results = {
-        'query': '["and", ["=","hash","e4fug294hf3293hf9348g3804hg3084h"],["=","latest-report?",true]]'}
+            'query': '["and", ["=","hash","e4fug294hf3293hf9348g3804hg3084h"],["=","latest-report?",true]]'
+        }
         results = mk_puppetdb_query(content)
         self.assertEqual(expected_results, results)
 
-    def test_single_query_with_operator(self):
+    def test_single_search_query_with_operator(self):
         content = {
             'query':
                 {
@@ -38,9 +41,108 @@ class CreatePuppetdbQueries(TestCase):
                     1: '["=","hash","e4fug294hf3293hf9348g3804hg3084h"]',
                 },
         }
-        expected_results = {'query': '["and", ["=","hash","e4fug294hf3293hf9348g3804hg3084h"]]'}
+        expected_results = {
+            'query': '["and", ["=","hash","e4fug294hf3293hf9348g3804hg3084h"]]'
+        }
         results = mk_puppetdb_query(content)
         self.assertEqual(expected_results, results)
 
+    def test_summarize_by_query(self):
+        content = {
+            'summarize-by': 'containing-class',
+        }
+        expected_results = {
+            'summarize-by': 'containing-class'
+        }
+        results = mk_puppetdb_query(content)
+        self.assertEqual(expected_results, results)
 
+    def test_summarize_by_query_with_single_search_query(self):
+        content = {
+            'query':
+                {
+                    1: '["=","certname","hostname.example.com"]'
+                },
+            'summarize-by': 'containing-class',
+        }
+        expected_results = {
+            'query': '["=","certname","hostname.example.com"]',
+            'summarize-by': 'containing-class'
+        }
+        results = mk_puppetdb_query(content)
+        self.assertEqual(expected_results, results)
 
+    def test_order_by_query(self):
+        content = {
+            'order-by':
+                {
+                    'order-field':
+                        {
+                            'field': 'report_timestamp',
+                            'order': 'desc',
+                        },
+                    'query-field':
+                        {
+                            'field': 'name'
+                        },
+                }
+        }
+        expected_results = {
+            'order-by': '[{"field":"report_timestamp","order":"desc"},{"field":"name"}]'
+        }
+        results = mk_puppetdb_query(content)
+        self.assertEqual(expected_results, results)
+
+    def test_order_by_query_with_single_search_query(self):
+        content = {
+            'query':
+                {
+                    1: '["=","certname","hostname.example.com"]'
+                },
+            'order-by':
+                {
+                    'order-field':
+                        {
+                            'field': 'report_timestamp',
+                            'order': 'desc',
+                        },
+                    'query-field':
+                        {
+                            'field': 'name'
+                        },
+                }
+        }
+        expected_results = {
+            'order-by': '[{"field":"report_timestamp","order":"desc"},{"field":"name"}]',
+            'query': '["=","certname","hostname.example.com"]'
+        }
+        results = mk_puppetdb_query(content)
+        self.assertEqual(expected_results, results)
+
+    def test_order_by_query_with_double_search_query_with_operator(self):
+        content = {
+            'query':
+                {
+                    'operator': 'or',
+                    1: '["=","certname","hostname1.example.com"]',
+                    2: '["=","certname","hostname2.example.com"]'
+                },
+            'order-by':
+                {
+                    'order-field':
+                        {
+                            'field': 'report_timestamp',
+                            'order': 'desc',
+                        },
+                    'query-field':
+                        {
+                            'field': 'name'
+                        },
+                }
+        }
+        expected_results = {
+            'query': '["and", ["=","certname","hostname1.example.com"],["=","certname","hostname2.example.com"]]',
+            'order-by': '[{"field":"report_timestamp","order":"desc"},{"field":"name"}]'
+        }
+        results = mk_puppetdb_query(content)
+        self.assertEqual(expected_results, results)
