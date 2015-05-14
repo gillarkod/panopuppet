17bae2826c19687e943fef3fdbf0d8d4c6f4cfb0
Takeshi Larsson <takeshi.p.larsson@gmail.com>
2015-05-14 09:20:56 +0200

    api also get their own directory

--- pano/views/views.py
+++ pano/views/views.py
@@ -1,5 +1,3 @@
-from django.contrib.auth.decorators import login_required
-
 class Echo(object):
     """An object that implements just the write method of the file-like
     interface.
@@ -8,8 +6,3 @@ class Echo(object):
     def write(self, value):
         """Write the value by returning it, instead of storing in a buffer."""
         return value
-
-
-@login_required
-def api(request):
-    return False
