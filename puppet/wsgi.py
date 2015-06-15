97d604471308d344bdc93cdad08a927d5746225b
propyless <takeshi.p.larsson@gmail.com>
2015-06-15 12:46:37 +0200

    add BASE_DIR to path

--- puppet/wsgi.py
+++ puppet/wsgi.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 
 import os
 import sys
-path = '/srv/repo/panopuppet'
-if path not in sys.path:
-    sys.path.insert(0, path)
+
+from puppet.settings import BASE_DIR
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "puppet.settings")
 
 from django.core.wsgi import get_wsgi_application
