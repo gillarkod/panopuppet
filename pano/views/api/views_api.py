2262879ec904ab397b68f397a71a71149c6e8088
Gregory Knight <gregory.knight@vocalocity.com>
2016-01-11 13:56:06 -0500

    packaging refactor

--- pano/views/api/views_api.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth.decorators import login_required
-
-__author__ = 'etaklar'
-
-
-@login_required
-def api(request):
-    return False
--- pano/views/logout.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth import logout
-from django.http import HttpResponseRedirect
-
-__author__ = 'etaklar'
-
-
-def logout_view(request):
-    logout(request)
-    return HttpResponseRedirect("/pano/dashboard")
